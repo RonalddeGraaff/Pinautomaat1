ac2cd595e0a54a9d6acca121448d6eca4ef8bf6d
rhm.degraaff@ziggo.nl <rhm.degraaff@ziggo.nl>
2020-05-12 03:22:16 +0200

    Hij werkt

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,14 +21,91 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        private double dSaldo = 1500;
         public MainWindow()
         {  
             InitializeComponent();
+            Verberg_bedragen();
+        }
+
+        private void Verberg_bedragen()
+        {
+            txt_10.Visibility = Visibility.Hidden;
+            txt_30.Visibility = Visibility.Hidden;
+            txt_50.Visibility = Visibility.Hidden;
+            txt_100.Visibility = Visibility.Hidden;
+            txt_150.Visibility = Visibility.Hidden;
+            txt_250.Visibility = Visibility.Hidden;
+            txt_OK.Visibility = Visibility.Visible;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            txtMessage.Text = "hello world"; 
+            boek_bedrag_af(250);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (pwPincode.Password == "4321")
+            {
+                txtMessage.Text = "Kies bedrag..";
+           
+                txt_10.Visibility = Visibility.Visible;
+                txt_30.Visibility = Visibility.Visible;
+                txt_50.Visibility = Visibility.Visible;
+                txt_100.Visibility = Visibility.Visible;
+                txt_150.Visibility = Visibility.Visible;
+                txt_250.Visibility = Visibility.Visible;
+                txt_OK.Visibility = Visibility.Hidden;
+                b_10.IsEnabled = true;
+                b_30.IsEnabled = true;
+                b_50.IsEnabled = true;
+                b_100.IsEnabled = true;
+                b_150.IsEnabled = true;
+                b_250.IsEnabled = true;
+                b_OK.IsEnabled = false;
+            }
+            else txtMessage.Text = "Onjuiste pincode!";
+        }
+        
+        private void boek_bedrag_af(double bedrag)
+        {
+            dSaldo -= bedrag;
+            txtMessage.Text = "Uw nieuwe saldo is € " + dSaldo + ". Nog een fijne dag!";
+            b_10.IsEnabled = false;
+            b_30.IsEnabled = false;
+            b_50.IsEnabled = false;
+            b_100.IsEnabled = false;
+            b_150.IsEnabled = false;
+            b_250.IsEnabled = false;
+            b_OK.IsEnabled = true;
+            Verberg_bedragen();
+            pwPincode.Password = "";
+        }
+
+        private void b_10_Click(object sender, RoutedEventArgs e)
+        {
+            boek_bedrag_af(10);
+        }
+
+        private void b_30_Click(object sender, RoutedEventArgs e)
+        {
+            boek_bedrag_af(30);
+        }
+
+        private void b_50_Click(object sender, RoutedEventArgs e)
+        {
+            boek_bedrag_af(50);
+        }
+
+        private void b_100_Click(object sender, RoutedEventArgs e)
+        {
+            boek_bedrag_af(100);
+        }
+
+        private void b_150_Click(object sender, RoutedEventArgs e)
+        {
+            boek_bedrag_af(150);
         }
     }
 }
